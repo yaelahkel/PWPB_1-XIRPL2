7a9c272b818a06abe4e56a37dc6dbf696d3c6cd2
yaelahkel <53940292+yaelahkel@users.noreply.github.com>
2019-08-12 21:34:32 +0700

    File Website PWPB

---
+++ index.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Jekyll v3.8.5">
+    <title>Tugas PWPB</title>
+    <link rel="canonical" href="https://getbootstrap.com/docs/4.3/examples/pricing/">
+    <link rel="shortcut icon" href="img/logo.png">
+    <!-- Bootstrap core CSS -->
+<link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <style>
+    
+      nav a:hover{
+        background: green;
+        text-decoration: none;
+      }
+    </style>
+    <!-- Custom styles for this template -->
+    <link href="pricing.css" rel="stylesheet">
+  </head>
+  <body>
+    <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 navbar-light border-bottom shadow-sm" style="background-color: rgb(66,181,73);">
+  <h5 class="my-0 mr-md-auto font-weight-normal text-light" style="font-family: forte; font-size: 32px; margin-left: 40px;">Kelompok 7</h5>
+  <nav class="my-2 my-md-0 mr-md-3">
+    <a class="p-2 text-light" href="tugas_pertama.php">Tugas pertama</a>
+    <a class="p-2 text-light" href="#">Tugas kedua</a>
+    <a class="p-2 text-light" href="#">Tugas ketiga</a>
+    <a class="p-2 text-light" href="#">Tugas keempat</a>
+  </nav>
+  <a class="btn btn-outline-light" href="index.php">Beranda</a>
+</div>
+
+<div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
+  <p class="display-4" style="font-family: forte;">PWPB</p>
+  <h4>Selamat datang di website pengumpulan tugas PWPB.</h4><br>
+  <p class="lead">Di sini tugas tugas pelajaran Pemrograman Web Perangkat Bergerak kami di kumpulkan,dari tugas pertama sampai tugas akhir.</p>
+  Kelompok 7 yang beranggotakan : Haikal Damar , Fera Dwi Lestari , Lusiana Amanah. <br><br>
+
+  <h4>I. Pengenalan</h4>
+
+World Wide Web adalah layanan internet yang paling populer saat ini. Internet mulai dikenal dan digunakan secara luas setelah adanya layanan WWW. WWW adalah halaman-halaman website yang dapat saling terkoneksi satu dengan lainnya (hyperlink) yang membentuk samudra belantara informasi. WWW berjalan dengan protokol HyperText Transfer Protokol (HTTP), dengan menggunakan HTTP dapat membuat user untuk mengakses suatu halaman web melalui protocol TCP/IP menjadi lebih mudah. <br><br>
+
+Halaman Web merupakan file teks murni (plain text) yang berisi sintaks-sintaks HTML yang dapat dibuka/dilihat/diterjemahkan dengan Internet Browser ketika Sintaks HTML mampu memuat konten text, gambar, audio, video, dan animasi. <br><br>
+
+  <img src="img/bahasa.jpg" width=" 60%" height="300px"><br><br><br>
+
+  <h4>II. Internet & Web Browser</h4>
+
+Browser adalah sebuah program aplikasi atau software yang me-request dokumen-dokumen dari komputer-komputer yang terkoneksi internet (server) di seluruh dunia, dan menampilkan informasi dari dokumen tersebut pada window browser. Browser menampilkan sesuai instruksi (format) HTML yang dibuat pada dokumen tersebut. <br><br>
+
+  <img src="img/codingan.jpg" width=" 60%" height="300px"><br><br><br>
+
+</div>
+
+<div class="container">
+  <div class="card-deck mb-3 text-center">
+     
+   
+  </div>
+
+  <footer class="pt-4 my-md-5 pt-md-5 border-top">
+    <div class="row">
+      <div class="col-12 col-md">
+        <img class="mb-2" src="/docs/4.3/assets/brand/bootstrap-solid.svg" alt="" width="24" height="24">
+        <small class="d-block mb-3 text-muted">&copy; 2019</small>
+        <p class="lead">#PWPB_XIRPL2</p>
+      </div>
+      <div class="col-6 col-md">
+        <h5>Features</h5>
+        <ul class="list-unstyled text-small">
+          <li><a class="text-muted" href="#">Cool stuff</a></li>
+          <li><a class="text-muted" href="#">Random feature</a></li>
+          <li><a class="text-muted" href="#">Team feature</a></li>
+          <li><a class="text-muted" href="#">Stuff for developers</a></li>
+          <li><a class="text-muted" href="#">Another one</a></li>
+          <li><a class="text-muted" href="#">Last time</a></li>
+        </ul>
+      </div>
+      <div class="col-6 col-md">
+        <h5>Resources</h5>
+        <ul class="list-unstyled text-small">
+          <li><a class="text-muted" href="#">Resource</a></li>
+          <li><a class="text-muted" href="#">Resource name</a></li>
+          <li><a class="text-muted" href="#">Another resource</a></li>
+          <li><a class="text-muted" href="#">Final resource</a></li>
+        </ul>
+      </div>
+      <div class="col-6 col-md">
+        <h5>About</h5>
+        <ul class="list-unstyled text-small">
+          <li><a class="text-muted" href="#">Team</a></li>
+          <li><a class="text-muted" href="#">Locations</a></li>
+          <li><a class="text-muted" href="#">Privacy</a></li>
+          <li><a class="text-muted" href="#">Terms</a></li>
+        </ul>
+      </div>
+    </div>
+  </footer>
+</div>
+</body>
+</html>
